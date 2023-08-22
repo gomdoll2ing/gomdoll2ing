5fd4a84d07202e0599f95f9bb16e1280a21d5495
gomdoll2ing <quddussla@naver.com>
2023-08-23 01:41:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -353,7 +353,7 @@ if radio_stock=='주식':
             </div>
             """
             
-            st.markdown(iframe_html, unsafe_allow_html=True)
+            st.sidebar.markdown(iframe_html, unsafe_allow_html=True)
             
             original_title = '<p style="font-family:Courier; color:Orange; font-size: 12px;">이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.</p>'
             st.sidebar.markdown(original_title, unsafe_allow_html=True)
