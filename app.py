924a21d45718244bc00d74e392503bb90028e5a4
gomdoll2ing <quddussla@naver.com>
2023-08-23 01:16:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -728,7 +728,13 @@ else:
     else:
         div_df = stock.get_index_fundamental(date='20230822')
         div_df = div_df.sort_values("배당수익률", ascending=False).head(20)
-        div_df["배당수익률"] = round(div_df["배당수익률"],2)
+        # 값을 6.38% 형식으로 바꾸는 함수 정의
+        def format_percentage(value):
+            formatted_value = "{:.2%}".format(round(value, 2))
+            return formatted_value
+        
+        # apply 함수를 사용하여 모든 값에 format_percentage 함수 적용
+        div_df = div_df.applymap(format_percentage)
         
         etf_dps = '<p style="font-family:Courier; color:Blue; font-size: 20px;">배당수익률 상위 10개 종목 매수 전략</p>'
         st.markdown(etf_dps, unsafe_allow_html=True)
