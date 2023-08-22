8c5c51892445d6f7eff9ce629daeceec48c460c2
gomdoll2ing <quddussla@naver.com>
2023-08-23 01:40:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -338,23 +338,22 @@ if radio_stock=='주식':
             #st.table(tmp_df)
             # 성공문구 + 풍선이 날리는 특수효과 
             st.sidebar.success("Filter Applied!")
-            html = """
-            <iframe src="https://coupa.ng/cd8kRY" width="200" height="240" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>
+            
+            iframe_html = """
+            <div style='display: flex;'>
+                <div style='flex: 33.33%; padding-right: 10px;'>
+                    <iframe src="https://coupa.ng/cd8kRY" width="120" height="240" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>
+                </div>
+                <div style='flex: 33.33%; padding-right: 10px;'>
+                    <iframe src="https://coupa.ng/cd8kY9" width="120" height="240" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>
+                </div>
+                <div style='flex: 33.33%;'>
+                    <iframe src="https://coupa.ng/cd8k1U" width="120" height="240" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>
+                </div>
+            </div>
             """
             
-            st.sidebar.markdown(html, unsafe_allow_html=True)
-            
-            html2 = """
-            <iframe src="https://coupa.ng/cd8kY9" width="200" height="240" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>
-            """
-            
-            st.sidebar.markdown(html2, unsafe_allow_html=True)
-            
-            html3 = """
-            <iframe src="https://coupa.ng/cd8k1U" width="200" height="240" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>
-            """
-            
-            st.sidebar.markdown(html3, unsafe_allow_html=True)
+            st.markdown(iframe_html, unsafe_allow_html=True)
             
             original_title = '<p style="font-family:Courier; color:Orange; font-size: 12px;">이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.</p>'
             st.sidebar.markdown(original_title, unsafe_allow_html=True)
