53bcbef3b363f823bbd014409d232d8c8b4e1bf0
gomdoll2ing <quddussla@naver.com>
2023-08-23 22:18:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -217,8 +217,8 @@ if radio_stock=='주식':
                     for code in code_list:
                         df_tmp = stock.get_market_ohlcv(str(past).replace("-",""),str(today).replace("-",""), code).dropna()
                         df_tmp["등락률"]=df_tmp["등락률"]/100
-                        #st.write(df_tmp)
                         df_tmp = df_tmp.rename(columns={"등락률":stock.get_market_ticker_name(code)})
+                        st.write(df_tmp)
                         df_cor = pd.concat([df_cor,df_tmp.loc[:,stock.get_market_ticker_name(code)]],1)
                         
                     sns.set(style="white")
